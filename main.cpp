188a9cd5de9a5bbd6d03ed2626db97060ec68529
davidthienprogramming <davidthienprogramming@gmail.com>
2015-04-08 09:25:56 -0700

    More updates

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
+#include <string>
 
 bool sortHighLow(int i, int j) {
 	return j < i;
@@ -11,35 +12,61 @@ int main(int argc, char** argv) {
 	std::vector<int> tradingValues;
 	std::vector<int> currentItems;
 	int startingValue;
-	int currentValue = startingValue;
+	int currentValue;
 	int numTrades = 0;
 	bool turnTime = true;
 	//temporary values
-	startingValue = 120;
+	startingValue = 10;
 	tradingValues.push_back(15);
-	tradingValues.push_back(8);
-	tradingValues.push_back(5);
-	tradingValues.push_back(62);
+	tradingValues.push_back(10);
+	tradingValues.push_back(3);
 	tradingValues.push_back(1);
 	
-	std::sort(tradingValues.begin(), tradingValues.end(), sortHighLow);
-	while(currentItems.empty()) {
-		std::cout << "test1" << std::endl;
-		currentValue = *currentItems.begin();
-		std::cout << "test2" << std::endl;
+	currentItems.push_back(startingValue);
+	while(!currentItems.empty()) {
+		currentValue = currentItems[0];
 		currentItems.erase(currentItems.begin());
-		std::vector<int>::iterator itr = tradingValues.begin();
-		while(itr != tradingValues.end()) {
-			if(*itr < currentValue){
-				std::cout << "test";
-				++numTrades;
+		for(std::vector<int>::iterator itr = tradingValues.begin(); itr != tradingValues.end();) {
+			while(currentValue > *itr){
 				currentValue -= *itr;
 				if(*itr != 1)
 					currentItems.push_back(*itr);
+				++numTrades;
+				std::cout << "currentValue = " << currentValue << " *itr = " << *itr << " numTrades = " << numTrades << std::endl;
+				std::string dummy;
+				std::getline(std::cin, dummy);
 			}
-			else
-				++itr;
+			++itr;
+			std::cout << "The currentItems list is: ";
+			for(std::vector<int>::iterator mtr = currentItems.begin(); mtr != currentItems.end(); ++mtr) {
+				std::cout << *mtr << " ";
+			}
+			std::cout << std::endl;
+			std::string dummy;
+			std::getline(std::cin, dummy);
 		}
 	}
+	
+	
+	
+	
+//	currentItems.push_back(startingValue);
+//	std::sort(tradingValues.begin(), tradingValues.end(), sortHighLow);
+//	while(!currentItems.empty()) {
+//		currentValue = currentItems[0];
+//		currentItems.erase(currentItems.begin());
+//		for(std::vector<int>::iterator itr = tradingValues.begin(); itr != tradingValues.end();) {
+//			std::cout << "The current value of the iterator is " << *itr << std::endl;
+//			std::cout << "The current value of currentValue is " << currentValue << std::endl;
+//			if(*itr < currentValue){
+//				++numTrades;
+//				currentValue -= *itr;
+//				if(*itr != 1)
+//					currentItems.push_back(*itr);
+//			}
+//			else
+//				++itr;
+//		}
+//	}
 	std::cout << numTrades;
 }
