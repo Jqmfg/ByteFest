886b12cbfd80a40a8ddeafceb5b6e779140d558d
davidthienprogramming <davidthienprogramming@gmail.com>
2015-04-09 10:11:09 -0700

    Working Program
    Still need to implement file input

--- main.cpp
+++ main.cpp
@@ -16,57 +16,24 @@ int main(int argc, char** argv) {
 	int numTrades = 0;
 	bool turnTime = true;
 	//temporary values
-	startingValue = 10;
-	tradingValues.push_back(15);
-	tradingValues.push_back(10);
-	tradingValues.push_back(3);
+	startingValue = 12849;
 	tradingValues.push_back(1);
 	
+	std::sort(tradingValues.begin(), tradingValues.end(), sortHighLow);
 	currentItems.push_back(startingValue);
 	while(!currentItems.empty()) {
 		currentValue = currentItems[0];
-		currentItems.erase(currentItems.begin());
+		++numTrades;
 		for(std::vector<int>::iterator itr = tradingValues.begin(); itr != tradingValues.end();) {
-			while(currentValue > *itr){
+			while(currentItems[0] > *itr && currentValue >= *itr){
 				currentValue -= *itr;
-				if(*itr != 1)
+				if(*itr != 1) {
 					currentItems.push_back(*itr);
-				++numTrades;
-				std::cout << "currentValue = " << currentValue << " *itr = " << *itr << " numTrades = " << numTrades << std::endl;
-				std::string dummy;
-				std::getline(std::cin, dummy);
+				}
 			}
 			++itr;
-			std::cout << "The currentItems list is: ";
-			for(std::vector<int>::iterator mtr = currentItems.begin(); mtr != currentItems.end(); ++mtr) {
-				std::cout << *mtr << " ";
-			}
-			std::cout << std::endl;
-			std::string dummy;
-			std::getline(std::cin, dummy);
 		}
+		currentItems.erase(currentItems.begin());
 	}
-	
-	
-	
-	
-//	currentItems.push_back(startingValue);
-//	std::sort(tradingValues.begin(), tradingValues.end(), sortHighLow);
-//	while(!currentItems.empty()) {
-//		currentValue = currentItems[0];
-//		currentItems.erase(currentItems.begin());
-//		for(std::vector<int>::iterator itr = tradingValues.begin(); itr != tradingValues.end();) {
-//			std::cout << "The current value of the iterator is " << *itr << std::endl;
-//			std::cout << "The current value of currentValue is " << currentValue << std::endl;
-//			if(*itr < currentValue){
-//				++numTrades;
-//				currentValue -= *itr;
-//				if(*itr != 1)
-//					currentItems.push_back(*itr);
-//			}
-//			else
-//				++itr;
-//		}
-//	}
 	std::cout << numTrades;
 }
